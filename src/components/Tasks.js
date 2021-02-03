dcff77660226a0e3d637ff37e96260b795fd8870
Solomii <kushniryksolomiya@gmail.com>
2021-02-03 19:20:00 +0200

    added delete

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,14 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: "Doctors Appointment",
-    day: "Feb 6th at 2:30pm",
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: "Food Shopping",
-    day: "Feb 5th at 2:30pm",
-    reminder: false,
-  },
-];
-
-const Tasks = () => {
+import Task from "./Task";
+const Tasks = ({ tasks, onDelete, onToggle }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <Task
+          key={task.id}
+          task={task}
+          onDelete={onDelete}
+          onToggle={onToggle}
+        />
       ))}
     </>
   );
